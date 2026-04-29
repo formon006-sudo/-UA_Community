c14e265647d03259f21cec34308386e549c1b140
formon006-sudo <formon006@gmail.com>
2026-04-29 14:58:00 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        // client.user.setActivity("Dota 2 | UA Community", { type: "PLAYING" });
         // Activity type number (0 = Playing).
         type: 0, 
       },
